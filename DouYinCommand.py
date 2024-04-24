3a8949d6cd706550815004e4634274468d457cb9
akafanfan <173358981@qq.com>
2024-04-24 15:57:15 +0800

    build project 1st

---
+++ DouYinCommand.py
@@ -0,0 +1,361 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import os
+import sys
+import json
+import yaml
+import time
+
+from apiproxy.douyin.douyin import Douyin
+from apiproxy.douyin.download import Download
+from apiproxy.douyin import douyin_headers
+from apiproxy.common import utils
+
+configModel = {
+    "link": [],
+    "path": os.getcwd(),
+    "music": True,
+    "cover": True,
+    "avatar": True,
+    "json": True,
+    "folderstyle": True,
+    "mode": ["post"],
+    "number": {
+        "post": 0,
+        "like": 0,
+        "allmix": 0,
+        "mix": 0,
+        "music": 0,
+    },
+    'database': True,
+    "increase": {
+        "post": False,
+        "like": False,
+        "allmix": False,
+        "mix": False,
+        "music": False,
+    },
+    "thread": 5,
+    "cookie": None
+
+}
+
+
+def argument():
+    parser = argparse.ArgumentParser(description='抖音批量下载工具 使用帮助')
+    parser.add_argument("--cmd", "-C", help="使用命令行(True)或者配置文件(False), 默认为False",
+                        type=utils.str2bool, required=False, default=False)
+    parser.add_argument("--link", "-l",
+                        help="作品(视频或图集)、直播、合集、音乐集合、个人主页的分享链接或者电脑浏览器网址, 可以设置多个链接(删除文案, 保证只有URL, https://v.douyin.com/kcvMpuN/ 或者 https://www.douyin.com/开头的)",
+                        type=str, required=False, default=[], action="append")
+    parser.add_argument("--path", "-p", help="下载保存位置, 默认当前文件位置",
+                        type=str, required=False, default=os.getcwd())
+    parser.add_argument("--music", "-m", help="是否下载视频中的音乐(True/False), 默认为True",
+                        type=utils.str2bool, required=False, default=True)
+    parser.add_argument("--cover", "-c", help="是否下载视频的封面(True/False), 默认为True, 当下载视频时有效",
+                        type=utils.str2bool, required=False, default=True)
+    parser.add_argument("--avatar", "-a", help="是否下载作者的头像(True/False), 默认为True",
+                        type=utils.str2bool, required=False, default=True)
+    parser.add_argument("--json", "-j", help="是否保存获取到的数据(True/False), 默认为True",
+                        type=utils.str2bool, required=False, default=True)
+    parser.add_argument("--folderstyle", "-fs", help="文件保存风格, 默认为True",
+                        type=utils.str2bool, required=False, default=True)
+    parser.add_argument("--mode", "-M", help="link是个人主页时, 设置下载发布的作品(post)或喜欢的作品(like)或者用户所有合集(mix), 默认为post, 可以设置多种模式",
+                        type=str, required=False, default=[], action="append")
+    parser.add_argument("--postnumber", help="主页下作品下载个数设置, 默认为0 全部下载",
+                        type=int, required=False, default=0)
+    parser.add_argument("--likenumber", help="主页下喜欢下载个数设置, 默认为0 全部下载",
+                        type=int, required=False, default=0)
+    parser.add_argument("--allmixnumber", help="主页下合集下载个数设置, 默认为0 全部下载",
+                        type=int, required=False, default=0)
+    parser.add_argument("--mixnumber", help="单个合集下作品下载个数设置, 默认为0 全部下载",
+                        type=int, required=False, default=0)
+    parser.add_argument("--musicnumber", help="音乐(原声)下作品下载个数设置, 默认为0 全部下载",
+                        type=int, required=False, default=0)
+    parser.add_argument("--database", "-d", help="是否使用数据库, 默认为True 使用数据库; 如果不使用数据库, 增量更新不可用",
+                        type=utils.str2bool, required=False, default=True)
+    parser.add_argument("--postincrease", help="是否开启主页作品增量下载(True/False), 默认为False",
+                        type=utils.str2bool, required=False, default=False)
+    parser.add_argument("--likeincrease", help="是否开启主页喜欢增量下载(True/False), 默认为False",
+                        type=utils.str2bool, required=False, default=False)
+    parser.add_argument("--allmixincrease", help="是否开启主页合集增量下载(True/False), 默认为False",
+                        type=utils.str2bool, required=False, default=False)
+    parser.add_argument("--mixincrease", help="是否开启单个合集下作品增量下载(True/False), 默认为False",
+                        type=utils.str2bool, required=False, default=False)
+    parser.add_argument("--musicincrease", help="是否开启音乐(原声)下作品增量下载(True/False), 默认为False",
+                        type=utils.str2bool, required=False, default=False)
+    parser.add_argument("--thread", "-t",
+                        help="设置线程数, 默认5个线程",
+                        type=int, required=False, default=5)
+    parser.add_argument("--cookie", help="设置cookie, 格式: \"name1=value1; name2=value2;\" 注意要加冒号",
+                        type=str, required=False, default='')
+    args = parser.parse_args()
+    if args.thread <= 0:
+        args.thread = 5
+
+    return args
+
+
+def yamlConfig():
+    curPath = os.path.dirname(os.path.realpath(sys.argv[0]))
+    yamlPath = os.path.join(curPath, "config.yml")
+    f = open(yamlPath, 'r', encoding='utf-8')
+    cfg = f.read()
+    configDict = yaml.load(stream=cfg, Loader=yaml.FullLoader)
+
+    try:
+        if configDict["link"] != None:
+            configModel["link"] = configDict["link"]
+    except Exception as e:
+        print("[  警告  ]:link未设置, 程序退出...\r\n")
+    try:
+        if configDict["path"] != None:
+            configModel["path"] = configDict["path"]
+    except Exception as e:
+        print("[  警告  ]:path未设置, 使用当前路径...\r\n")
+    try:
+        if configDict["music"] != None:
+            configModel["music"] = configDict["music"]
+    except Exception as e:
+        print("[  警告  ]:music未设置, 使用默认值True...\r\n")
+    try:
+        if configDict["cover"] != None:
+            configModel["cover"] = configDict["cover"]
+    except Exception as e:
+        print("[  警告  ]:cover未设置, 使用默认值True...\r\n")
+    try:
+        if configDict["avatar"] != None:
+            configModel["avatar"] = configDict["avatar"]
+    except Exception as e:
+        print("[  警告  ]:avatar未设置, 使用默认值True...\r\n")
+    try:
+        if configDict["json"] != None:
+            configModel["json"] = configDict["json"]
+    except Exception as e:
+        print("[  警告  ]:json未设置, 使用默认值True...\r\n")
+    try:
+        if configDict["folderstyle"] != None:
+            configModel["folderstyle"] = configDict["folderstyle"]
+    except Exception as e:
+        print("[  警告  ]:folderstyle未设置, 使用默认值True...\r\n")
+    try:
+        if configDict["mode"] != None:
+            configModel["mode"] = configDict["mode"]
+    except Exception as e:
+        print("[  警告  ]:mode未设置, 使用默认值post...\r\n")
+    try:
+        if configDict["number"]["post"] != None:
+            configModel["number"]["post"] = configDict["number"]["post"]
+    except Exception as e:
+        print("[  警告  ]:post number未设置, 使用默认值0...\r\n")
+    try:
+        if configDict["number"]["like"] != None:
+            configModel["number"]["like"] = configDict["number"]["like"]
+    except Exception as e:
+        print("[  警告  ]:like number未设置, 使用默认值0...\r\n")
+    try:
+        if configDict["number"]["allmix"] != None:
+            configModel["number"]["allmix"] = configDict["number"]["allmix"]
+    except Exception as e:
+        print("[  警告  ]:allmix number未设置, 使用默认值0...\r\n")
+    try:
+        if configDict["number"]["mix"] != None:
+            configModel["number"]["mix"] = configDict["number"]["mix"]
+    except Exception as e:
+        print("[  警告  ]:mix number未设置, 使用默认值0...\r\n")
+    try:
+        if configDict["number"]["music"] != None:
+            configModel["number"]["music"] = configDict["number"]["music"]
+    except Exception as e:
+        print("[  警告  ]:music number未设置, 使用默认值0...\r\n")
+    try:
+        if configDict["database"] != None:
+            configModel["database"] = configDict["database"]
+    except Exception as e:
+        print("[  警告  ]:database未设置, 使用默认值False...\r\n")
+    try:
+        if configDict["increase"]["post"] != None:
+            configModel["increase"]["post"] = configDict["increase"]["post"]
+    except Exception as e:
+        print("[  警告  ]:post 增量更新未设置, 使用默认值False...\r\n")
+    try:
+        if configDict["increase"]["like"] != None:
+            configModel["increase"]["like"] = configDict["increase"]["like"]
+    except Exception as e:
+        print("[  警告  ]:like 增量更新未设置, 使用默认值False...\r\n")
+    try:
+        if configDict["increase"]["allmix"] != None:
+            configModel["increase"]["allmix"] = configDict["increase"]["allmix"]
+    except Exception as e:
+        print("[  警告  ]:allmix 增量更新未设置, 使用默认值False...\r\n")
+    try:
+        if configDict["increase"]["mix"] != None:
+            configModel["increase"]["mix"] = configDict["increase"]["mix"]
+    except Exception as e:
+        print("[  警告  ]:mix 增量更新未设置, 使用默认值False...\r\n")
+    try:
+        if configDict["increase"]["music"] != None:
+            configModel["increase"]["music"] = configDict["increase"]["music"]
+    except Exception as e:
+        print("[  警告  ]:music 增量更新未设置, 使用默认值False...\r\n")
+    try:
+        if configDict["thread"] != None:
+            configModel["thread"] = configDict["thread"]
+    except Exception as e:
+        print("[  警告  ]:thread未设置, 使用默认值5...\r\n")
+    try:
+        if configDict["cookies"] != None:
+            cookiekey = configDict["cookies"].keys()
+            cookieStr = ""
+            for i in cookiekey:
+                cookieStr = cookieStr + i + "=" + configDict["cookies"][i] + "; "
+            configModel["cookie"] = cookieStr
+    except Exception as e:
+        pass
+    try:
+        if configDict["cookie"] != None:
+            configModel["cookie"] = configDict["cookie"]
+    except Exception as e:
+        pass
+
+
+def main():
+    start = time.time()  # 开始时间
+
+    args = argument()
+
+    if args.cmd:
+        configModel["link"] = args.link
+        configModel["path"] = args.path
+        configModel["music"] = args.music
+        configModel["cover"] = args.cover
+        configModel["avatar"] = args.avatar
+        configModel["json"] = args.json
+        configModel["folderstyle"] = args.folderstyle
+        if args.mode == None or args.mode == []:
+            args.mode = []
+            args.mode.append("post")
+        configModel["mode"] = list(set(args.mode))
+        configModel["number"]["post"] = args.postnumber
+        configModel["number"]["like"] = args.likenumber
+        configModel["number"]["allmix"] = args.allmixnumber
+        configModel["number"]["mix"] = args.mixnumber
+        configModel["number"]["music"] = args.musicnumber
+        configModel["database"] = args.database
+        configModel["increase"]["post"] = args.postincrease
+        configModel["increase"]["like"] = args.likeincrease
+        configModel["increase"]["allmix"] = args.allmixincrease
+        configModel["increase"]["mix"] = args.mixincrease
+        configModel["increase"]["music"] = args.musicincrease
+        configModel["thread"] = args.thread
+        configModel["cookie"] = args.cookie
+    else:
+        yamlConfig()
+
+    if configModel["link"] == []:
+        return
+
+    if configModel["cookie"] is not None and configModel["cookie"] != "":
+        douyin_headers["Cookie"] = configModel["cookie"]
+
+    configModel["path"] = os.path.abspath(configModel["path"])
+    print("[  提示  ]:数据保存路径 " + configModel["path"])
+    if not os.path.exists(configModel["path"]):
+        os.mkdir(configModel["path"])
+
+    dy = Douyin(database=configModel["database"])
+    dl = Download(thread=configModel["thread"], music=configModel["music"], cover=configModel["cover"],
+                  avatar=configModel["avatar"], resjson=configModel["json"],
+                  folderstyle=configModel["folderstyle"])
+
+    for link in configModel["link"]:
+        print("--------------------------------------------------------------------------------")
+        print("[  提示  ]:正在请求的链接: " + link + "\r\n")
+        url = dy.getShareLink(link)
+        key_type, key = dy.getKey(url)
+        if key_type == "user":
+            print("[  提示  ]:正在请求用户主页下作品\r\n")
+            data = dy.getUserDetailInfo(sec_uid=key)
+            nickname = ""
+            if data is not None and data != {}:
+                nickname = utils.replaceStr(data['user']['nickname'])
+
+            userPath = os.path.join(configModel["path"], nickname)
+            if not os.path.exists(userPath):
+                os.mkdir(userPath)
+
+            for mode in configModel["mode"]:
+                print("--------------------------------------------------------------------------------")
+                print("[  提示  ]:正在请求用户主页模式: " + mode + " 昵称：" + nickname +"\r\n")
+                if mode == 'post' or mode == 'like':
+                    datalist = dy.getUserInfo(key, mode, 35, configModel["number"][mode], configModel["increase"][mode])
+                    if datalist is not None and datalist != []:
+                        modePath = os.path.join(userPath, mode)
+                        if not os.path.exists(modePath):
+                            os.mkdir(modePath)
+                        dl.userDownload(awemeList=datalist, savePath=modePath)
+                elif mode == 'mix':
+                    mixIdNameDict = dy.getUserAllMixInfo(key, 35, configModel["number"]["allmix"])
+                    if mixIdNameDict is not None and mixIdNameDict != {}:
+                        for mix_id in mixIdNameDict:
+                            print(f'[  提示  ]:正在下载合集 [{mixIdNameDict[mix_id]}] 中的作品\r\n')
+                            mix_file_name = utils.replaceStr(mixIdNameDict[mix_id])
+                            datalist = dy.getMixInfo(mix_id, 35, 0, configModel["increase"]["allmix"], key)
+                            if datalist is not None and datalist != []:
+                                modePath = os.path.join(userPath, mode)
+                                if not os.path.exists(modePath):
+                                    os.mkdir(modePath)
+                                dl.userDownload(awemeList=datalist, savePath=os.path.join(modePath, mix_file_name))
+                                print(f'[  提示  ]:合集 [{mixIdNameDict[mix_id]}] 中的作品下载完成\r\n')
+        elif key_type == "mix":
+            print("[  提示  ]:正在请求单个合集下作品\r\n")
+            datalist = dy.getMixInfo(key, 35, configModel["number"]["mix"], configModel["increase"]["mix"], "")
+            if datalist is not None and datalist != []:
+                mixname = utils.replaceStr(datalist[0]["mix_info"]["mix_name"])
+                mixPath = os.path.join(configModel["path"], "mix_" + mixname + "_" + key)
+                if not os.path.exists(mixPath):
+                    os.mkdir(mixPath)
+                dl.userDownload(awemeList=datalist, savePath=mixPath)
+        elif key_type == "music":
+            print("[  提示  ]:正在请求音乐(原声)下作品\r\n")
+            datalist = dy.getMusicInfo(key, 35, configModel["number"]["music"], configModel["increase"]["music"])
+
+            if datalist is not None and datalist != []:
+                musicname = utils.replaceStr(datalist[0]["music"]["title"])
+                musicPath = os.path.join(configModel["path"], "music_" + musicname + "_" + key)
+                if not os.path.exists(musicPath):
+                    os.mkdir(musicPath)
+                dl.userDownload(awemeList=datalist, savePath=musicPath)
+        elif key_type == "aweme":
+            print("[  提示  ]:正在请求单个作品\r\n")
+            datanew, dataraw = dy.getAwemeInfo(key)
+            if datanew is not None and datanew != {}:
+                datalist = []
+                datalist.append(datanew)
+                awemePath = os.path.join(configModel["path"], "aweme")
+                if not os.path.exists(awemePath):
+                    os.mkdir(awemePath)
+                dl.userDownload(awemeList=datalist, savePath=awemePath)
+        elif key_type == "live":
+            print("[  提示  ]:正在进行直播解析\r\n")
+            live_json = dy.getLiveInfo(key)
+            if configModel["json"]:
+                livePath = os.path.join(configModel["path"], "live")
+                if not os.path.exists(livePath):
+                    os.mkdir(livePath)
+                live_file_name = utils.replaceStr(key + live_json["nickname"])
+                # 保存获取到json
+                print("[  提示  ]:正在保存获取到的信息到result.json\r\n")
+                with open(os.path.join(livePath, live_file_name + ".json"), "w", encoding='utf-8') as f:
+                    f.write(json.dumps(live_json, ensure_ascii=False, indent=2))
+                    f.close()
+
+    end = time.time()  # 结束时间
+    print('\n' + nickname +' [下载完成]:总耗时: %d分钟%d秒\n' % (int((end - start) / 60), ((end - start) % 60)))  # 输出下载用时时间
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,312 @@
-DEMO
+
+💾 本程序中大量使用了ChatGPT生成的代码，如有bug，请在issue中提出。
+
+👉 想要了解如何使用ChatGPT写程序，可以加群交流。
+
+# 抖音批量下载工具功能介绍
+
+💁 支持个人主页链接、作品分享链接、抖音直播Web链接、合集链接、音乐(原声)集合链接
+
+🔽 支持单个作品下载、主页作品下载、主页喜欢下载、直播解析、单个合集下载、主页所有合集下载、音乐(原声)集合下载
+
+📹 下载视频、视频封面、音乐、头像
+
+💦 去水印下载
+
+⏭️ 自动跳过已下载
+
+👉 支持指定下载作品数量
+
+🔗 多线程下载、多链接下载
+
+📈 增量更新与数据持久化到数据库, 保存每条作品信息到数据库, 并根据数据库是否存在来增量请求下载
+
+
+# 申明
+
+本项目只作为学习用途, 切勿他用.
+有任何问题可以进群交流。
+
+<img src="img/fuye.png" width = "200" />
+
+# 使用截图
+
+![DouYinCommand1](img/DouYinCommand1.jpg)
+![DouYinCommand2](img/DouYinCommand2.jpg)
+![DouYinCommandl ive](img/DouYinCommandlive.jpg)
+![DouYinCommand download](img/DouYinCommanddownload.jpg)
+![DouYinCommand download detail](img/DouYinCommanddownloaddetail.jpg)
+
+# 使用方法
+
+- 支持的地址格式, 形如
+
+```
+抖音链接:
+1. 作品(视频或图集)、直播、合集、音乐集合、个人主页    https://v.douyin.com/BugmVVD/    
+抖音网页版浏览器URL:
+2. 单个视频             https://www.douyin.com/video/6915675899241450760
+3. 单个图集             https://www.douyin.com/note/7014363562642623777
+4. 用户主页             https://www.douyin.com/user/MS4wLjABAAAA06y3Ctu8QmuefqvUSU7vr0c_ZQnCqB0eaglgkelLTek
+5. 单个合集             https://www.douyin.com/collection/7208829743762769975
+6. 音乐(原声)下的视频     https://www.douyin.com/music/7149936801028131598
+7. 直播                https://live.douyin.com/759547612580                     
+```
+
+# 抖音批量下载工具
+
+批量下载有两种方式运行, 配置文件和命令行
+
+默认使用配置文件方式，在配置文件`config.example.yml`中修改内容后，将文件另存为`config.yml`。
+
+### 安装依赖
+
+windows用户下载 Release文件夹 中的 [DouYinCommand.exe]文件运行
+windows用户本地有`python3.9`环境, 也可按照linux与mac用户的方式运行
+
+linux与mac用户下载本项目, 在本地`python3.9`环境中运行, 首先需要安装依赖, 安装命令
+
+```
+cd /path/to/douyin-downloader
+python -m pip install -r requirements.txt
+```
+
+### 配置文件方式
+
+配置文件名必须叫 `config.yml`, 并将其放在DouYinCommand.py或者DouYinCommand.exe同一个目录下
+
+直接运行DouYinCommand.py或者DouYinCommand.exe, 无需在命令中加入任何参数, 所有参数都从配置文件中读取
+
+基本配置示例[config.yml](./config.yml), 请自己登录网页版抖音后F12获取cookie
+
+
+### 命令行方式
+
+运行示例:
+
+- 获取帮助信息
+
+```
+windows用户:
+.\DouYinCommand.exe -h
+linux与mac用户:
+python DouYinCommand.py -h
+```
+
+- 参数介绍
+
+```
+-h, --help                       展示帮助信息
+--cmd CMD, -C CMD                使用命令行(True)或者配置文件(False), 默认为False
+--link LINK, -l LINK             作品(视频或图集)、直播、合集、音乐集合、个人主页的分享链接或者电脑浏览器网址, 可以设置多个链接
+                                 (删除文案, 保证只有URL, https://v.douyin.com/kcvMpuN/ 或者 https://www.douyin.com/开头的)
+--path PATH, -p PATH             下载保存位置, 默认当前文件位置
+--music MUSIC, -m MUSIC          是否下载视频中的音乐(True/False), 默认为True
+--cover COVER, -c COVER          是否下载视频的封面(True/False), 默认为True, 当下载视频时有效
+--avatar AVATAR, -a AVATAR       是否下载作者的头像(True/False), 默认为True
+--json JSON, -j JSON             是否保存获取到的数据(True/False), 默认为True
+--folderstyle FOLDERSTYLE, -fs FOLDERSTYLE   文件保存风格, 默认为True
+--mode MODE, -M MODE             link是个人主页时, 设置下载发布的作品(post)或喜欢的作品(like)或者用户所有合集(mix), 默认为post,  可以设置多种模式
+--postnumber POSTNUMBER          主页下作品下载个数设置, 默认为0 全部下载
+--likenumber LIKENUMBER          主页下喜欢下载个数设置, 默认为0 全部下载
+--allmixnumber ALLMIXNUMBER      主页下合集下载个数设置, 默认为0 全部下载
+--mixnumber MIXNUMBER            单个合集下作品下载个数设置, 默认为0 全部下载
+--musicnumber MUSICNUMBER        音乐(原声)下作品下载个数设置, 默认为0 全部下载
+--database DATABASE, -d DATABASE 是否使用数据库, 默认为True 使用数据库; 如果不使用数据库, 增量更新不可用
+--postincrease POSTINCREASE      是否开启主页作品增量下载(True/False), 默认为False
+--likeincrease LIKEINCREASE      是否开启主页喜欢增量下载(True/False), 默认为False
+--allmixincrease ALLMIXINCREASE  是否开启主页合集增量下载(True/False), 默认为False
+--mixincrease MIXINCREASE        是否开启单个合集下作品增量下载(True/False), 默认为False
+--musicincrease MUSICINCREASE    是否开启音乐(原声)下作品增量下载(True/False), 默认为False
+--thread THREAD, -t THREAD       设置线程数, 默认5个线程
+--cookie COOKIE                  设置cookie, 格式: "name1=value1; name2=value2;" 注意要加冒号
+```
+
+- 多链接多模式混合下载, 可以传入多个链接和多个模式(post、like、mix)
+
+```
+windows用户:
+.\DouYinCommand.exe -C True `
+  -l https://live.douyin.com/759547612580 `
+  -l https://v.douyin.com/BugmVVD/ `
+  -l https://v.douyin.com/BugrFTN/ `
+  -l https://v.douyin.com/B72pdU5/ `
+  -l https://v.douyin.com/B72QgDw/ `
+  -l https://v.douyin.com/AJp8D3f/ `
+  -l https://v.douyin.com/B38oovu/ `
+  -l https://v.douyin.com/S6YMNXs/ `
+  -p C:\project\test `
+  -M post `
+  -M like `
+  -M mix `
+  --postnumber 5 `
+  --likenumber 5 `
+  --allmixnumber 1 `
+  --mixnumber 5 `
+  --musicnumber 5 `
+  --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+
+linux与mac用户:
+python DouYinCommand.py -C True \
+  -l https://live.douyin.com/759547612580 \
+  -l https://v.douyin.com/BugmVVD/ \
+  -l https://v.douyin.com/BugrFTN/ \
+  -l https://v.douyin.com/B72pdU5/ \
+  -l https://v.douyin.com/B72QgDw/ \
+  -l https://v.douyin.com/AJp8D3f/ \
+  -l https://v.douyin.com/B38oovu/ \
+  -l https://v.douyin.com/S6YMNXs/ \
+  -p /path/to/downdir \
+  -M post \
+  -M like \
+  -M mix \
+  --postnumber 5 \
+  --likenumber 5 \
+  --allmixnumber 1 \
+  --mixnumber 5 \
+  --musicnumber 5 \
+  --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载单个作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/kcvMpuN/ -p C:\project\test --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/kcvMpuN/ -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载主页全部作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/kcvSCe9/ -p C:\project\test --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/kcvSCe9/ -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载主页前n个作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/kcvSCe9/ -p C:\project\test --postnumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/kcvSCe9/ -p /path/to/downdir --postnumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载主页全部喜欢
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/kcvSCe9/ -p C:\project\test -M like --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/kcvSCe9/ -p /path/to/downdir -M like --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载主页前n个喜欢
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/kcvSCe9/ -p C:\project\test -M like --likenumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/kcvSCe9/ -p /path/to/downdir -M like --likenumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载单个合集全部作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/B3J63Le/ -p C:\project\test --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/B3J63Le/ -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载单个合集前n个作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/B3J63Le/ -p C:\project\test --mixnumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/B3J63Le/ -p /path/to/downdir --mixnumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载主页全部合集下所有作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/B38oovu/ -p C:\project\test -M mix --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/B38oovu/ -p /path/to/downdir -M mix --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载主页前n个合集下所有作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/B38oovu/ -p C:\project\test -M mix --allmixnumber 2 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/B38oovu/ -p /path/to/downdir -M mix --allmixnumber 2 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载音乐(原声)集合下所有作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/S6YMNXs/ -p C:\project\test --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/S6YMNXs/ -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 下载音乐(原声)集合下前n个作品
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/S6YMNXs/ -p C:\project\test --musicnumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/S6YMNXs/ -p /path/to/downdir --musicnumber 30 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 关闭头像下载, cover, music json数据也是一样的设置对应选项为 False
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/kcvSCe9/ -p C:\project\test -a False --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/kcvSCe9/ -p /path/to/downdir -a False --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 多线程设置, 默认5个线程, 可以自己调节线程数
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://v.douyin.com/kcvSCe9/ -p C:\project\test -t 8 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://v.douyin.com/kcvSCe9/ -p /path/to/downdir -t 8 --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+- 直播推流地址解析
+
+```
+windows用户:
+.\DouYinCommand.exe -C True -l https://live.douyin.com/802939216127 -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+或者
+.\DouYinCommand.exe -C True -l https://v.douyin.com/SnXMoh2/ -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+linux与mac用户:
+python DouYinCommand.py -C True -l https://live.douyin.com/802939216127 -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+或者
+python DouYinCommand.py -C True -l https://v.douyin.com/SnXMoh2/ -p /path/to/downdir --cookie "msToken=xxx; ttwid=xxx; odin_tt=xxx; passport_csrf_token=xxx; sid_guard=xxx;"
+```
+
+
+# 鸣谢
+
+本项目部分思路来自[TikTokDownload](https://github.com/Johnserf-Seed/TikTokDownload)
+
+
+
+
+# License
+
+[MIT](https://opensource.org/licenses/MIT) 
+
